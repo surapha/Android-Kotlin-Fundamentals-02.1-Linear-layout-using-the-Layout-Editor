86dd57f1fb2d25e85076f5ebc83462659be9de94
USER PC <surapha.nu@rmuti.ac.th>
2020-07-20 21:33:17 +0700

    Step 2: Attach the click listener to the DONE Button

--- app/src/main/java/android/example/com/aboutme/MainActivity.kt
+++ app/src/main/java/android/example/com/aboutme/MainActivity.kt
@@ -3,6 +3,7 @@ package android.example.com.aboutme
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
@@ -10,13 +11,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)
+        }
     }
-}
-private fun addNickname(view: View) {
-    val editText = findViewById<EditText>(R.id.nickname_edit)
-    val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
-    nicknameTextView.text = editText.text
-    editText.visibility = View.GONE
-    view.visibility = View.GONE
-    nicknameTextView.visibility = View.VISIBLE
+
+    private fun addNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+    }
+
 }
