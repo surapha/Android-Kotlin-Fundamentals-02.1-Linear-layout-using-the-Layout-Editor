37d7d67e8c58b02cbf94bf0c6c2416142c21b2c8
USER PC <surapha.nu@rmuti.ac.th>
2020-07-20 21:19:10 +0700

    7. Task: Add a click listener to the DONE button Step 1: Add a click listener

--- app/src/main/java/android/example/com/aboutme/MainActivity.kt
+++ app/src/main/java/android/example/com/aboutme/MainActivity.kt
@@ -2,10 +2,21 @@ package android.example.com.aboutme
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+}
+private fun addNickname(view: View) {
+    val editText = findViewById<EditText>(R.id.nickname_edit)
+    val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+    nicknameTextView.text = editText.text
+    editText.visibility = View.GONE
+    view.visibility = View.GONE
+    nicknameTextView.visibility = View.VISIBLE
 }
